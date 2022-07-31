9d312e03b8bae00245b918baef92a42945658e30
Willian <willian_98@outlook.com>
2022-07-31 17:06:30 -0400

    Fix icon bug

--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -3,6 +3,10 @@ import {
   FontAwesomeIconProps
 } from '@fortawesome/react-fontawesome'
 
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
+
 export default function Icon({
   color = 'var(--dk-text-color)',
   size = 'lg',
